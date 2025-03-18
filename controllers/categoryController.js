3c00e8d56c31999b9fbfdb0e55a0ef7f67aa6d0f
Veuz Concepts <aswinjith.ks@gmail.com>
2025-03-18 16:39:33 +0530

    fixes

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,30 +1,27 @@
-import db from "../models/index.js";
-import AppError from "../utils/appError.js";
-import asyncHandler from "../utils/asyncHandler.js";
+import db from '../models/index.js';
+import AppError from '../utils/appError.js';
+import asyncHandler from '../utils/asyncHandler.js';
 
 const { Category } = db;
 
 export const getAllCategories = asyncHandler(async (req, res) => {
-  const userId = req.admin;
-
-  const categories = await Category.findAll({ where: { userId } });
-
+  const categories = await Category.findAll();
   res.json({
     success: true,
-    data: categories,
+    data: categories
   });
 });
 
 export const createCategory = asyncHandler(async (req, res) => {
-  const { name, image } = req.body;
-  const userId = req.admin;
+  const { name, image } = req.body; 
   if (!name) {
     throw new AppError("Category name is required", 400);
   }
-  const category = await Category.create({ name, image, userId });
+  const category = await Category.create({ name, image });
   res.status(201).json({
     success: true,
     message: "Category created successfully",
     data: category,
   });
 });
+
--- models/Category.js
+++ models/Category.js
@@ -4,28 +4,23 @@ const CategoryModel = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        notEmpty: { msg: "Category name cannot be empty" },
+        notEmpty: {
+          msg: "Category name cannot be empty",
+        },
       },
     },
     image: {
       type: DataTypes.STRING,
       allowNull: true,
       validate: {
-        isUrl: { msg: "Image must be a valid URL" },
-      },
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: "Users",
-        key: "id",
+        isUrl: {
+          msg: "Image must be a valid URL",
+        },
       },
     },
   });
 
   Category.associate = (models) => {
-    Category.belongsTo(models.User, { foreignKey: "userId" });
     Category.hasMany(models.FoodItem);
   };
 
